13355e66497639d811aa9ce2fb41be3ee82970b1
Drew Lazzeri <dlazzeri1@gmail.com>
2017-10-17 15:06:08 -0700

    Started to parse names and positions

---
+++ cast.py
@@ -0,0 +1,59 @@
+template = """<div class="w3-card-2 w3-quarter">
+  <header class="w3-container">
+    <h3>{name}</h3>
+  </header>
+  <div class="w3-container">
+    <p> {role}</p>
+  </div>
+</div>
+"""
+crew_positions = """
+Director
+Producer
+Executive Producer
+Writer/Screenplay
+1st AD
+Director of Photography
+1st AC
+2nd AC
+Gaffer
+Key Grip
+Sound Op
+Boom Op
+Key MUA
+BTS Photography
+PA 1
+PA 2
+PA 3
+Editor
+Composer 1
+Composer 2
+Composer 3
+Craft Services
+"""
+
+crew_names = """
+Brian "Sham" Green
+Aldo Billingslea
+Wm. Derek Grasty
+Wm. Derek Grasty
+Jay Raja
+Gavin Murray
+Ferguson Rogan
+Stephen Bartlett
+Ryan Nelson
+Kat.
+Adam Drakewolf
+Steven Lagosh
+Whittany Robinson
+Nathan Adams
+Lee Harold
+Skylar Adams
+Thomas Ariniello
+Brian Green
+Peder B Helland
+Vidal Garcia
+Derek Thomas
+Grasty Family
+"""
+print "hello"
