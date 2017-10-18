92dcdef185349d9bb92292f2fcff1e6cfb050eeb
Drew Lazzeri <dlazzeri1@gmail.com>
2017-10-18 13:05:44 -0700

    Cast and Crew, Autoclose trailer

--- cast.py
+++ cast.py
@@ -1,4 +1,4 @@
-template = """<div class="w3-card-2 w3-quarter">
+template = """<div class="w3-card-2 w3-quarter w3-margin cast-card bloody">
   <header class="w3-container">
     <h3>{name}</h3>
   </header>
@@ -7,8 +7,30 @@ template = """<div class="w3-card-2 w3-quarter">
   </div>
 </div>
 """
-crew_positions = """
-Director
+
+cast_positions = """Margo
+Mingus
+Samantha
+Parker
+Carla
+Luna"""
+
+cast_names = """Brenda Wooley
+Aldo B.
+Nikki Green
+Michael Curry
+Natalie Kelly
+Courtney Land"""
+
+print """<div class="w3-container" > <h3 class="w3-wide">Cast</h3>"""
+cast_pairs = zip(cast_names.split("\n"), cast_positions.split("\n"))
+for name, position in cast_pairs:
+    print(template.format(name=name, role=position))
+
+print "</div>"
+
+
+crew_positions = """Director
 Producer
 Executive Producer
 Writer/Screenplay
@@ -29,11 +51,9 @@ Editor
 Composer 1
 Composer 2
 Composer 3
-Craft Services
-"""
+Craft Services"""
 
-crew_names = """
-Brian "Sham" Green
+crew_names = """Brian "Sham" Green
 Aldo Billingslea
 Wm. Derek Grasty
 Wm. Derek Grasty
@@ -54,6 +74,12 @@ Brian Green
 Peder B Helland
 Vidal Garcia
 Derek Thomas
-Grasty Family
-"""
-print "hello"
+Grasty Family"""
+
+print """<div class="w3-container" > <h3 class="w3-wide">Crew </h3>"""
+
+crew_pairs = zip(crew_positions.split("\n"), crew_names.split("\n"))
+for name, position in crew_pairs:
+    print(template.format(name=name, role=position))
+
+print "</div>"
